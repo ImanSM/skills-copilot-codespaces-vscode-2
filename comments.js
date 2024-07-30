595a9635e2f36cb6280a3e3bf9f1e842ff84b9ea
Iman Santamaria <59697317+ImanSM@users.noreply.github.com>
2024-07-30 11:40:02 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -8,7 +8,7 @@
 
 const express = require('express');
 const mongoose = require('mongoose');
-const Comment = require('./comment.js');
+const Comment = require('./comments.js');
 const app = express();
 const port = 3000;
 
