e0e5dc646ffe4e08dc98e8e41be68d2fc262fc4a
Iman Santamaria <59697317+ImanSM@users.noreply.github.com>
2024-07-30 11:18:13 +0000

    Copilot third commit

---
+++ comment.js
@@ -0,0 +1,62 @@
+// Create web server
+// Create a comment
+// Get all comments
+// Get a comment by id
+// Delete a comment
+// Update a comment
+// Create a comment schema
+
+const express = require('express');
+const mongoose = require('mongoose');
+const Comment = require('./comment.js');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/comment', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
+
+app.post('/comments', async (req, res) => {
+  const { author, content } = req.body;
+  const comment = new Comment({ author, content });
+  await comment.save();
+  res.json(comment);
+});
+
+app.get('/comments', async (req, res) => {
+  const comments = await Comment.find();
+  res.json(comments);
+});
+
+app.get('/comments/:id', async (req, res) => {
+  const { id } = req.params;
+  const comment = await Comment.findById(id);
+  res.json(comment);
+});
+
+app.delete('/comments/:id', async (req, res) => {
+  const { id } = req.params;
+  await Comment.findByIdAndDelete(id);
+  res.json({ message: 'Comment deleted' });
+});
+
+app.put('/comments/:id', async (req, res) => {
+  const { id } = req.params;
+  const { author, content } = req.body;
+  const comment = await Comment.findByIdAndUpdate(
+    id,
+    { author, content },
+    { new: true }
+  );
+  res.json(comment);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+// Comment Schema
